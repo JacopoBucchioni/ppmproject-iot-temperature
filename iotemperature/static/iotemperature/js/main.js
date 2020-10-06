f69c4a04bca3036c81c6ce99657f229838516df6
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2020-10-06 12:38:32 +0200

    Update main.js

--- iotemperature/static/iotemperature/js/main.js
+++ iotemperature/static/iotemperature/js/main.js
@@ -1,6 +1,7 @@
+/* Javascript Code */
+
 $(document).ready(function() {
-  $.ajaxSetup({ cache: false }); // This part addresses an IE bug.  without it, IE will only load the first number and will never refresh
-  var my_refresh = setInterval(function() {
-    $('#mysensors').load('/');
-  }, 10000); // the "1000"
+  setInterval(function(){
+    $('#main').load('/');
+  }, 7000);
 });
