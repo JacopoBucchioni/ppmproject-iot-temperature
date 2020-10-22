d2dba0c661f81c9b7ec1546f433039c59ed3475e
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2020-10-22 18:34:25 +0200

    Update myCharts.js

--- iotemperature/static/iotemperature/js/myCharts.js
+++ iotemperature/static/iotemperature/js/myCharts.js
@@ -2,6 +2,7 @@ var xlabels = [];
 var temps = [];
 var hum = [];
 
+/*
 for (i = 0; i < misurazioni.length; i++) {
     xlabels.push(misurazioni[i]["fields"].date);
     temps.push(misurazioni[i]["fields"].temperature);
@@ -10,6 +11,8 @@ for (i = 0; i < misurazioni.length; i++) {
 //console.log(xlabels);
 //console.log(temps);
 //console.log(hum);
+*/
+
 
 
 var config = {
@@ -18,64 +21,128 @@ var config = {
         labels: xlabels,
         datasets: [
             {
-                label: 'Temperatura in °C',
+                label: 'Temperatura',
                 fill: false,
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
                 data: temps,
+                yAxisID: 'y-axis-1',
             },
 
             {
-                label: 'Umidità in %',
+                label: 'Umidità',
                 fill: false,
                 backgroundColor: 'rgb(54, 162, 235)',
                 borderColor: 'rgb(54, 162, 235)',
                 data: hum,
+                yAxisID: 'y-axis-2'
             }
         ]
     },
 
     options: {
         responsive: true,
+
+        hoverMode: 'index',
+        stacked: false,
+
         scales: {
             xAxes: [{
                 display: true
             }],
             yAxes: [{
                 display: true,
+                type: 'linear',
+                position: 'left',
+                id: 'y-axis-1',
                 ticks: {
-                    beginAtZero: true
+                    //beginAtZero: true,
+                    callback: function (value, index, values){
+                        return value + '°C';
+                    }
                 }
+            },{
+                display: true,
+                type: 'linear',
+                position: 'right',
+                id: 'y-axis-2',
+                ticks: {
+                    //beginAtZero: true,
+                    callback: function (value, index, values){
+                        return value + '%';
+                    }
+                },
+
+                // grid line settings
+                gridLines: {
+                    drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
+
             }]
         }
     }
 
 };
 
-
 window.onload = function () {
     var ctx = document.getElementById('mychart').getContext('2d');
-    window.myChart = new Chart(ctx, config);
+    window.myLine = Chart.Line(ctx, config);
 };
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     var date = new Date().toISOString();
-    date = date.substring(0, date.length-8);
+    date = date.substring(0, date.length - 8);
     $("#inizio")[0].max = date;
     $("#fine")[0].max = date;
 
 
     $("#bottoneGrafico").click(function () {
+        var id_sensore = $("#sensori")[0].value;
         var inizio = $("#inizio")[0].value;
         var fine = $("#fine")[0].value;
-        if (inizio<=fine){
-            console.log("data inizio: "+inizio);
-            console.log("data fine: "+fine);
-        }
-        else {
-            alert("Data Inizio Supera Data Fine");
-            $("#inizio")[0].value = "";
-            $("#fine")[0].value = "";
+        if (id_sensore != "Seleziona Sensore") {
+            if (inizio && fine) {
+                if (inizio <= fine) {
+                    console.log("ID sensore selezionato: " + id_sensore);
+                    console.log("data inizio: " + inizio);
+                    console.log("data fine: " + fine);
+
+
+                    for (i = 0; i < misurazioni.length; i++) {
+                        if (misurazioni[i]["fields"].sensor == id_sensore && misurazioni[i]["fields"].date<=fine && misurazioni[i]["fields"].date>=inizio) {
+                            xlabels.push(misurazioni[i]["fields"].date);
+                            temps.push(misurazioni[i]["fields"].temperature);
+                            hum.push(misurazioni[i]["fields"].humidity);
+
+                            window.myLine.update();
+                        }
+                    }
+
+                } else {
+                    alert("Data Inizio Supera Data Fine");
+                    $("#inizio")[0].value = "";
+                    $("#fine")[0].value = "";
+                }
+            } else {
+                console.log("ID sensore selezionato: " + id_sensore);
+                console.log("data inizio: " + inizio);
+                console.log("data fine: " + fine);
+
+
+                for (i = 0; i < misurazioni.length; i++) {
+                    if (misurazioni[i]["fields"].sensor == id_sensore) {
+                        xlabels.push(misurazioni[i]["fields"].date);
+                        temps.push(misurazioni[i]["fields"].temperature);
+                        hum.push(misurazioni[i]["fields"].humidity);
+
+                        window.myLine.update();
+                    }
+                }
+
+            }
+        } else {
+            alert("Non hai selezionato alcun Sesore");
         }
 
     });
