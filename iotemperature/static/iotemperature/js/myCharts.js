24164fab884e2f18af381d98bdaa13653ff3e31e
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2020-10-22 16:33:20 +0200

    Update myCharts.js

--- iotemperature/static/iotemperature/js/myCharts.js
+++ iotemperature/static/iotemperature/js/myCharts.js
@@ -1,38 +1,83 @@
+var xlabels = [];
+var temps = [];
+var hum = [];
+
+for (i = 0; i < misurazioni.length; i++) {
+    xlabels.push(misurazioni[i]["fields"].date);
+    temps.push(misurazioni[i]["fields"].temperature);
+    hum.push(misurazioni[i]["fields"].humidity);
+}
+//console.log(xlabels);
+//console.log(temps);
+//console.log(hum);
+
+
 var config = {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            datasets: [
-                {
-                    label: 'My First dataset',
-                    fill: false,
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [5, 12, 31, 45, 30, 22, 10],
-                },
+    type: 'line',
+    data: {
+        labels: xlabels,
+        datasets: [
+            {
+                label: 'Temperatura in °C',
+                fill: false,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: temps,
+            },
 
-                {
-                    label: 'My Second dataset',
-                    fill: false,
-                    backgroundColor: 'rgb(54, 162, 235)',
-                    borderColor: 'rgb(54, 162, 235)',
-                    data: [17, 15, 50, 22, 31, 44, 19],
-                }
-            ]
-        },
-
-        options: {
-            responsive: true,
-            scales: {
-                xAxes: [{display: true}],
-                yAxes: [{display: true}]
+            {
+                label: 'Umidità in %',
+                fill: false,
+                backgroundColor: 'rgb(54, 162, 235)',
+                borderColor: 'rgb(54, 162, 235)',
+                data: hum,
             }
-        }
+        ]
+    },
 
-    };
+    options: {
+        responsive: true,
+        scales: {
+            xAxes: [{
+                display: true
+            }],
+            yAxes: [{
+                display: true,
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+
+};
 
 
 window.onload = function () {
-        var ctx = document.getElementById('mychart').getContext('2d');
-        window.myChart = new Chart(ctx, config);
-    };
+    var ctx = document.getElementById('mychart').getContext('2d');
+    window.myChart = new Chart(ctx, config);
+};
+
+$(document).ready(function() {
+    var date = new Date().toISOString();
+    date = date.substring(0, date.length-8);
+    $("#inizio")[0].max = date;
+    $("#fine")[0].max = date;
+
+
+    $("#bottoneGrafico").click(function () {
+        var inizio = $("#inizio")[0].value;
+        var fine = $("#fine")[0].value;
+        if (inizio<=fine){
+            console.log("data inizio: "+inizio);
+            console.log("data fine: "+fine);
+        }
+        else {
+            alert("Data Inizio Supera Data Fine");
+            $("#inizio")[0].value = "";
+            $("#fine")[0].value = "";
+        }
+
+    });
+
+});
