444d01d0a6dda34c864e2b5319e111255ba9eba9
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2020-10-31 16:29:20 +0100

    Update home.js

--- iotemperature/static/iotemperature/js/home.js
+++ iotemperature/static/iotemperature/js/home.js
@@ -1,8 +1,9 @@
 //Configuration variables
 var updateInterval = 1000 //in ms
-var mydata;
 
+var online;
 var container;
+
 
 function getData(callback) {
     $.ajax({
@@ -11,11 +12,16 @@ function getData(callback) {
 
         success: function (response) {
             //console.log(response);
-            if (JSON.stringify(mydata) === JSON.stringify(response)) { }
+            /*
+            if (JSON.stringify(online) === JSON.stringify(response)) { }
             else {
-                mydata = response;
+                online = response;
                 callback();
             }
+             */
+            online = response;
+            callback();
+
         },
         error: function (error) {
             console.log(error);
@@ -25,15 +31,15 @@ function getData(callback) {
 
 function appendData() {
     //console.log('mydata', mydata);
-    if (mydata) {
+    if (online) {
         //TODO: hide <p> with big text ""NESSUN SENSORE ONLINE"
 
-        for (var i in mydata) {
+        for (var i in online) {
             var sensorCard = $("#" + i);
-            var date = new Date(mydata[i]["date"]).toLocaleString();
+            var date = new Date(online[i]["date"]);
             //console.log('mydata date',mydata[i]["date"]);
             //console.log('js date', date);
-            if ((new Date().getTime() - date.getTime()) > 300000) {  // se la data dell'ultima misurazione (provoniente da online->response->mydata) ha un delta di più di 5 min (300000ms) dall'ora corrente cancella la card del sensore che viene considerato offline
+            if ((new Date().getTime() - date.getTime()) > 60000) {  // se la data dell'ultima misurazione (provoniente da online->response->mydata) ha un delta di più di 5 min (300000ms) dall'ora corrente cancella la card del sensore che viene considerato offline
 
                 if (sensorCard.length) {
                     console.log("l'elemento esite e viene cancellato");
@@ -45,23 +51,23 @@ function appendData() {
                 if (sensorCard.length) {
                     console.log("l'elemento esite già e viene solo aggiornato");
 
-                    $("#" + i + "-data").html('temperatura: ' + mydata[i]["temperature"] + ' ℃ <br> umidità: ' + mydata[i]["humidity"] + ' %');
-                    $("#" + i + "-last-update").html('Last update: ' + date);
+                    $("#" + i + "-data").html('temperatura: ' + online[i]["temperature"] + ' ℃ <br> umidità: ' + online[i]["humidity"] + ' %');
+                    $("#" + i + "-last-update").html('Last update: ' + date.toLocaleString());
 
                 } else {
                     console.log("l'elemento viene creato per la prima volta");
 
-                    if (mydata[i]["sensor"]["friendlyName"]) {
+                    if (online[i]["sensor"]["friendlyName"]) {
                         container.append('<div class="row justify-content-center" id="' + i + '">\n' +
                             '                <div class="col-12 col-md-10 col-lg-5">\n' +
                             '                    <div class="card mt-3 text-center shadow rounded">\n' +
                             '                        <div class="card-body" id="mis-card-body">\n' +
-                            '                            <h3 class="card-title" id="sensor-name"><a href="/home/sensor/' + mydata[i]["sensor"]["id"] + '">' + mydata[i]["sensor"]["friendlyName"] + '</a></h3>\n' +
-                            '                            <p class="card-text" id="IPAddress"><small class="text-muted">' + mydata[i]["IPAddress"] + '</small></p>\n' +
-                            '                            <p class="card-text" id="' + i + '-data" style="margin-top: 5px; margin-bottom: 15px;">temperatura: ' + mydata[i]["temperature"] + ' ℃ <br> umidità: ' + mydata[i]["humidity"] + ' % </p>\n' +
+                            '                            <h3 class="card-title" id="sensor-name"><a href="/home/sensor/' + online[i]["sensor"]["id"] + '">' + online[i]["sensor"]["friendlyName"] + '</a></h3>\n' +
+                            '                            <p class="card-text" id="IPAddress"><small class="text-muted">' + online[i]["IPAddress"] + '</small></p>\n' +
+                            '                            <p class="card-text" id="' + i + '-data" style="margin-top: 5px; margin-bottom: 15px;">temperatura: ' + online[i]["temperature"] + ' ℃ <br> umidità: ' + online[i]["humidity"] + ' % </p>\n' +
                             '                        </div>\n' +
                             '                        <div class="card-footer">\n' +
-                            '                            <p class="card-text"><small class="text-muted" id="' + i + '-last-update">Last update: ' + date + '</small></p>\n' +
+                            '                            <p class="card-text"><small class="text-muted" id="' + i + '-last-update">Last update: ' + date.toLocaleString() + '</small></p>\n' +
                             '                        </div>\n' +
                             '                    </div>\n' +
                             '                </div>\n' +
@@ -71,11 +77,11 @@ function appendData() {
                             '                <div class="col-12 col-md-10 col-lg-5">\n' +
                             '                    <div class="card mt-3 text-center shadow rounded">\n' +
                             '                        <div class="card-body" id="mis-card-body">\n' +
-                            '                            <h3 class="card-title" id="sensor-name"><a href="/home/sensor/' + mydata[i]["sensor"]["id"] + '">' + mydata[i]["IPAddress"] + '</a></h3>\n' +
-                            '                            <p class="card-text" id="' + i + '-data" style="margin-top: 5px; margin-bottom: 15px;">temperatura: ' + mydata[i]["temperature"] + ' ℃ <br> umidità: ' + mydata[i]["humidity"] + ' % </p>\n' +
+                            '                            <h3 class="card-title" id="sensor-name"><a href="/home/sensor/' + online[i]["sensor"]["id"] + '">' + online[i]["IPAddress"] + '</a></h3>\n' +
+                            '                            <p class="card-text" id="' + i + '-data" style="margin-top: 5px; margin-bottom: 15px;">temperatura: ' + online[i]["temperature"] + ' ℃ <br> umidità: ' + online[i]["humidity"] + ' % </p>\n' +
                             '                        </div>\n' +
                             '                        <div class="card-footer">\n' +
-                            '                            <p class="card-text"><small class="text-muted" id="' + i + '-last-update">Last update: ' + date + '</small></p>\n' +
+                            '                            <p class="card-text"><small class="text-muted" id="' + i + '-last-update">Last update: ' + date.toLocaleString() + '</small></p>\n' +
                             '                        </div>\n' +
                             '                    </div>\n' +
                             '                </div>\n' +
@@ -83,7 +89,6 @@ function appendData() {
                     }
                 }
             }
-
         }
     } else {
         console.log("NESSUN SENSORE ONLINE")
