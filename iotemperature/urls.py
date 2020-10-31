19fae197e9bae02a8a4c9701cc5451de6c2c3c44
Jacopo Bucchioni <jacopo.bucchioni@stud.unifi.it>
2020-10-31 12:38:46 +0100

    Update urls.py

--- iotemperature/urls.py
+++ iotemperature/urls.py
@@ -6,13 +6,13 @@ from . import views
 urlpatterns = [
     path('', views.home, name='index'),
     path('home/', views.home, name='home'),
-    # path('home/', views.get_mis, name='home'),
     path('home/sensor/<int:pk>/', views.sensor_view, name='sensor_view'),
 
     path('sensors/', views.sensors_list, name='sensors_list'),
     path('sensors/edit/<int:pk>/', views.sensor_edit, name='sensor_edit'),
 
     path('charts/', views.charts, name='charts'),
+
     # path('cluster/', views.cluster_list, name='cluster'),
 
 
@@ -21,5 +21,4 @@ urlpatterns = [
     path('getData/', views.get_data, name='get_data'),
     path('home/getData/', views.get_data, name='get_data'),
     path('home/sensor/<int:pk>/getData/', views.get_sensor_data, name='get_sensor_data'),
-
 ]
