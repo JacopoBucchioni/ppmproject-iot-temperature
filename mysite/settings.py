3865abe4872da1ac7e0710dbf847e8ee554775d4
JacopoBucchioni <bucchionjacopo96@gmail.com>
2021-12-29 12:33:50 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from configparser import ConfigParser
+config = ConfigParser()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^zqipnh)==68a@^6t_h-#o_1t#fzjb-d#ym^d##ix6f@(7+0u8'
+config.read('./.credentials.ini')
+SECRET_KEY = config.get('section', 'SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
